738ef83e20fd96924de666b2186b963e47c01b20
reon <nr0807525@gmail.com>
2025-08-11 14:09:20 +0700

    made form-container

--- social/views.py
+++ social/views.py
@@ -1,21 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from .forms import PageForm
 from django.http import HttpResponse
 
 def social(request):
-    return render(request, 'socialnetwork.html')
-
-class PageCreateView(View):
-    def get(self, request):
-        form = PageForm()
-        return render(request, 'socialnetwork.html', {"form": form})
-    
-    def post(self, request):
+    if request.method == 'POST':
         form = PageForm(request.POST)
         if form.is_valid():
             form.save()
-        return render(request, 'socialnetwork.html', {"form": form})
-    
-
-page_create = PageCreateView.as_view()
+            return redirect('social')
+    else:
+        form = PageForm()
+    return render(request, 'socialnetwork.html', {'form': form})
+        

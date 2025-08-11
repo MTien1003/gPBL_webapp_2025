bb0bd9e8ae8ed9b1e18c009d004331598599679a
reon <nr0807525@gmail.com>
2025-08-11 10:54:01 +0700

    first migration

--- social/models.py
+++ social/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+
+class Page(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    body = models.TextField(max_length=2000)
+    created_at = models.TimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.created_at
--- social/views.py
+++ social/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 from django.views import View
+from .forms import PageForm
 from django.http import HttpResponse
-
 
 def social(request):
     return render(request, 'socialnetwork.html')
+
+class PageCreateView(View):
+    def get(self, request):
+        form = PageForm()
+        return render(request, 'socialnetwork.html', {"form": form})
+    
+    def post(self, request):
+        form = PageForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return render(request, 'socialnetwork.html', {"form": form})
+    
+
+page_create = PageCreateView.as_view()

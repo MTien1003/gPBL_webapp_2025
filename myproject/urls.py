3d3926f23fd55a2b5e545f4e777eb46483d34bfd
reon <nr0807525@gmail.com>
2025-08-11 10:01:44 +0700

    made path social/

--- myproject/urls.py
+++ myproject/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('home.urls')),
-
+    path("social/", include("social.urls")),
 ]
 
 # Serve static files during development
